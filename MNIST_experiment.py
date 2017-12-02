Fixed accuracy calculation, CPI/Libor plots

MNIST_experiment.py
@@ -55,37 +55,91 @@ results = []
 
 # Run optimizations
 for lam in lam_list:
+	filename = 'mnist_lam-%e.out' % lam
 
-	# Get model
-	model = IIDEncoding(p_in, p_out, hidden_units, lr, opt, lam, penalty_groups, nonlinearity = 'sigmoid', task = 'classification')
+	if os.path.isfile(filename):
+		print('Skipping experiment with lam = %e' % lam)
 
-	# Run experiment
-	train_loss, val_loss, weights = run_experiment(model, X_train, Y_train, X_val, Y_val, 
-		nepoch, mbsize = mbsize)
-	
-	# Perform variable selection
-	GC_est = np.zeros(len(penalty_groups) - 1)
-	adj_GC_est = np.zeros(len(penalty_groups) - 1)
-	for i, (start, end) in enumerate(list(zip(penalty_groups[:-1], penalty_groups[1:]))):
-		GC_est[i] = np.linalg.norm(weights[:, start:end], ord = 2)
-		adj_GC_est[i] = np.linalg.norm(weights[:, start:end], ord = 2) / (end - start)
+		with open(filename, 'rb') as f:
+			results_dict = pickle.load(f)
+
+	else:
+		print('Training network with lam = %f' % lam)
+
+		# Get model
+		model = IIDEncoding(p_in, p_out, hidden_units, lr, opt, lam, penalty_groups, nonlinearity = 'sigmoid', task = 'classification')
+
+		# Run experiment
+		train_loss, val_loss, train_accuracy, val_accuracy, weights = run_experiment(model, X_train, Y_train, X_val, Y_val, 
+			nepoch, mbsize = mbsize, loss_check = 1)
 		
-	# Save results
-	results_dict = {
-		'train_loss': train_loss,
-		'val_loss': val_loss,
-		'weights': weights,
-		'GC_est': GC_est,
-		'adj_GC_est': adj_GC_est
-	}
+		# Perform variable selection
+		GC_est = np.zeros(len(penalty_groups) - 1)
+		adj_GC_est = np.zeros(len(penalty_groups) - 1)
+		for i, (start, end) in enumerate(list(zip(penalty_groups[:-1], penalty_groups[1:]))):
+			GC_est[i] = np.linalg.norm(weights[:, start:end], ord = 2)
+			adj_GC_est[i] = np.linalg.norm(weights[:, start:end], ord = 2) / (end - start)
+			
+		# Save results
+		results_dict = {
+			'train_loss': train_loss,
+			'val_loss': val_loss,
+			'train_accuracy': train_accuracy,
+			'val_accuracy': val_accuracy,
+			'weights': weights,
+			'GC_est': GC_est,
+			'adj_GC_est': adj_GC_est
+		}
+
+		with open(filename, 'wb') as f:
+			pickle.dump(results_dict, f)
+	
 	results.append(results_dict)
 
 with open('mnist.out', 'wb') as f:
 	pickle.dump(results, f)
 
-for lam, result in zip(lam_list, results):
+# for lam, result in zip(lam_list, results):
+# 	GC = result['GC_est']
+# 	plt.imshow(np.reshape(GC, (28, 28)), cmap = 'gray')
+# 	plt.title(r'$\lambda = %.4f$' % lam)
+# 	plt.show()
+
+# for lam, result in zip(lam_list, results):
+# 	plt.plot(result['train_loss'])
+# 	plt.title(r'$\lambda = %.4f$' % lam)
+# 	plt.show()
+
+cmap = plt.cm.gray
+cmap.set_bad(color = 'cyan')
+fig, axarr = plt.subplots(2, 4, sharey = 'row', figsize = (10, 5))
+for i, (lam, result) in enumerate(list(zip(lam_list[:-1], results[:-1]))):
 	GC = result['GC_est']
-	plt.imshow(np.reshape(GC, (28, 28)), cmap = 'gray')
-	plt.title('lam = %f' % lam)
-	# plt.show()
-	plt.savefig('mnist_lam_%f.png' % lam)
+	train_accuracy = result['train_accuracy']
+	val_accuracy = result['val_accuracy']
+
+	# Plot GC image
+	ax = axarr[0, i]
+	masked = np.ma.masked_where(GC == 0, GC)
+	ax.imshow(np.reshape(masked, (28, 28)), cmap = cmap)
+	ax.get_xaxis().set_ticks([])
+	ax.get_yaxis().set_ticks([])
+
+	# Plot accuracy
+	ax = axarr[1, i]
+	ax.plot(train_accuracy, label = 'Train')
+	ax.plot(val_accuracy, label = 'Validation')
+	ax.legend(loc = 'center right', fontsize = 10)
+	ax.set_xlabel('Epoch')
+
+for i, label in enumerate(['GC Metric', 'Accuracy']):
+	ax = axarr[i, 0]
+	ax.set_ylabel(label)
+
+for i, lam in enumerate(lam_list[:-1]):
+	ax = axarr[0, i]
+	ax.set_title(r'$\lambda = %.4f$' % lam)
+
+plt.subplots_adjust(left = 0.1)
+plt.tight_layout()
+plt.show()

data_processing.py
@@ -61,5 +61,5 @@ def whiten_data_cholesky(X):
 
 def normalize(X):
 	X_centered = X - np.mean(X, axis = 0)
-	sigma = np.sqrt(np.var(X))
+	sigma = np.sqrt(np.var(X, axis = 0))
 	return np.divide(X_centered, sigma)

model_iid.py
@@ -97,7 +97,7 @@ class IIDEncoding:
 		Y_var = Variable(torch.from_numpy(Y).long())
 		Y_pred = self._forward(X)
 		maxes, inds = torch.max(Y_pred, dim = 1)
-		return torch.sum(Y_var == inds).data[0] / float(len(inds))
+		return (Y_var == inds).float().sum().data[0] / float(len(inds))
 
 	def _forward(self, X):
 		X_var = Variable(torch.from_numpy(X).float())

model_iid_ridge.py
@@ -63,7 +63,7 @@ class RidgeNetwork:
 		Y_var = Variable(torch.from_numpy(Y).long())
 		Y_pred = self._forward(X)
 		maxes, inds = torch.max(Y_pred, dim = 1)
-		return torch.sum(Y_var == inds).data[0] / float(len(inds))
+		return (Y_var == inds).float().sum().data[0] / float(len(inds))
 
 	def train(self, X, Y):
 		loss = self._loss(X, Y)